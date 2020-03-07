505f7e2e7d30a5ff923fae23afab8b34a58457da
Spencer Hurley <hurle156@umn.edu>
2020-03-07 12:17:27 -0600

    everything was working it is your fault if if stops

--- createtables.sql
+++ createtables.sql
@@ -1,8 +1,8 @@
-
+drop table "brewery_tbl";
 CREATE TABLE "brewery_tbl" (
     "brewery_id" int   NOT NULL,
     "brewery_name" char(100)   NOT NULL,
-    "city" char(100)   NOT NULL,
+    "city" char(100)   NULL,
     "state" char(100)    NULL,
     "country" char(100)   NOT NULL,
     "beer_count" INT   NOT NULL,
