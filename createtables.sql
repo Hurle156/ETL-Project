852fdf093fab840b811b9c9e702efb193a045a8b
Spencer Hurley <hurle156@umn.edu>
2020-03-07 11:53:13 -0600

    working tables with query

--- createtables.sql
+++ createtables.sql
@@ -1,24 +1,22 @@
+
 CREATE TABLE "brewery_tbl" (
-    "brewery_id" char(10)   NOT NULL,
-    "brewery_name" char(30)   NOT NULL,
-    "city" char(30)   NOT NULL,
-    "state" char(2)   NOT NULL,
-    "country" char(30)   NOT NULL,
+    "brewery_id" int   NOT NULL,
+    "brewery_name" char(100)   NOT NULL,
+    "city" char(100)   NOT NULL,
+    "state" char(100)    NULL,
+    "country" char(100)   NOT NULL,
     "beer_count" INT   NOT NULL,
-    "rating" FLOAT   NOT NULL,
+    "rating" FLOAT    NULL,
     CONSTRAINT "pk_brewery_tbl" PRIMARY KEY (
         "brewery_id"
      )
 );
+
 CREATE TABLE "beer_tbl" (
-    "id" INT   NOT NULL,
-    "name" char(30)   NOT NULL,
-    "syle" char(30)   NOT NULL,
-    "ABV" FLOAT   NOT NULL,
-    "brewery_id" char(10)   NOT NULL,
-    CONSTRAINT "pk_beer_tbl" PRIMARY KEY (
-        "id"
-     )
+    "beer_name" char(100)   NULL,
+    "style" char(100)  NULL,
+    "ABV" FLOAT   NULL,
+    "brewery_id" int   NOT NULL
 );
-ALTER TABLE "brewery_tbl" ADD CONSTRAINT "fk_brewery_tbl_brewery_id" FOREIGN KEY("brewery_id")
-REFERENCES "beer_tbl" ("brewery_id");
+
+
